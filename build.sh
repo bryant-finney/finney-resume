365e8548e3ea672c40cb1c6e7d34d6b15833ef73
Bryant Finney <bryant.finney@ierustech.com>
2019-01-17 15:53:52 -0600

    build.sh: completed. re-runs latex while 'rerun' is output

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 datestr="`date +%Y-%m-%d`"
 latex --output-format=pdf main.tex > build.log
+while grep -n rerun build.log > /dev/null
+do
+  echo "Build incomplete; rerunning..."
+  latex --output-format=pdf main.tex > build.log
+done
+echo "Build finished"
 mv main.pdf ${datestr}_finney-resume.pdf
 xdg-open ${datestr}_finney-resume.pdf
